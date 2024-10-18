af5ac6291aca9799b1e18939e1a2a1daca57ea40
rellow <dean.sullivan@unh.edu>
2024-10-18 15:39:13 -0400

    patch hidapi config

--- rpi_msp430/steps/build_hidapi.sh
+++ rpi_msp430/steps/build_hidapi.sh
@@ -6,6 +6,7 @@ trap "echo ❌ Failed to build HIDAPI" ERR
 cd hidapi
 ./bootstrap || ./bootstrap # It installs ltmain.sh in the wrong place the first time, then it copies into the right folder the second time...
 ./configure
+sed '/AM_INIT_AUTOMAKE/{n;s/^/\#/}' configure.ac
 make
 cd ..
 
